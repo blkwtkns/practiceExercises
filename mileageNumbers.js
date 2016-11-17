2c0b0ce3f054e092d928e62eb76d0200ba0d31a5
Blake Watkins <iamsoblake@gmail.com>
2016-11-16 17:22:56 -0800

    mileage exercise

---
+++ mileageNumbers.js
@@ -0,0 +1,11 @@
+function isInteresting(number, awesomePhrases) {
+  // Go to town!
+}
+
+
+Test.assertEquals(isInteresting(3, [1337, 256]), 0);
+Test.assertEquals(isInteresting(1336, [1337, 256]), 1);
+Test.assertEquals(isInteresting(1337, [1337, 256]), 2);
+Test.assertEquals(isInteresting(11208, [1337, 256]), 0);
+Test.assertEquals(isInteresting(11209, [1337, 256]), 1);
+Test.assertEquals(isInteresting(11211, [1337, 256]), 2);
